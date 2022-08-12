05844ae7b74619bf352105105b567d392fbb6e3f
OmarAbdullNasser <65048313+OmarAbdullNasser@users.noreply.github.com>
2022-08-12 22:20:32 +0200

    Update footer.vue

--- src/views/footer.vue
+++ src/views/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
                 <small>
-                    You can find the code repo on <a href="https://github.com/Flopet17/quiz-app">Github</a>. Feel free to make any pull requests to improve the aplication.
+                    You can find the code repo on <a href="https://github.com/OmarAbdullNasser/QuizzApp">Github</a>. Feel free to make any pull requests to improve the aplication.
                 </small>
                 <p>Built with <span>&hearts;</span> by <a href="https://protifilo.herokuapp.com/">Omar AbdullNasser</a></p>
           </footer>
